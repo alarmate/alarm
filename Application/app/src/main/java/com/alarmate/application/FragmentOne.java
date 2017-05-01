507556bda4e06122057eba05c414dbc388c189d1
lsj234027 <lsj234027@gmail.com>
2017-05-01 22:18:31 +0900

    workiing about 'SlidingUpPanel'

--- Application/app/src/main/java/com/alarmate/application/FragmentOne.java
+++ Application/app/src/main/java/com/alarmate/application/FragmentOne.java
@@ -1,14 +1,17 @@
 package com.alarmate.application;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 /**
  * Created by seongjinlee on 2017. 5. 1..
@@ -29,13 +32,12 @@ public class FragmentOne extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         CoordinatorLayout layout = (CoordinatorLayout)inflater.inflate(R.layout.fragment_one, container, false);
-        Button addBtn = (Button)layout.findViewById(R.id.add_alarm);
-
-
+        FloatingActionButton addBtn = (FloatingActionButton)layout.findViewById(R.id.add_alarm);
+        final Context context = this.getContext();
         View.OnClickListener addAlarmListener = new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-
+                Toast.makeText(context, "Add Alarm", Toast.LENGTH_SHORT).show();
             }
         };
 
