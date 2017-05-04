ccee7696f9a4bd2382f17bdb0be46548a258a2a1
tinyhhj <krwsim@gmail.com>
2017-05-05 01:54:32 +0900

    리스트어댑터 추가

--- Application/app/src/main/java/com/alarmate/application/AlarmItem.java
+++ Application/app/src/main/java/com/alarmate/application/AlarmItem.java
@@ -13,49 +13,80 @@ public class AlarmItem {
     private long aId;
     private int aStatus;
     private Calendar aCal;
-    private SimpleDateFormat sdf;
+    private SimpleDateFormat aSdf;
+    private String aTitle;
+    private String aContent;
+    private String aEnable;
+    private String aRepeat;
+    private String aRingtone;
 
     //알람 초기화
     public AlarmItem() {
         aId = 0;
         aStatus = 0;
         aCal = Calendar.getInstance();
-        sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm");
+        aTitle = "";
+        aContent = "";
+        aSdf = new SimpleDateFormat("yyyy/MM/dd HH:mm");
+        aEnable = "N";
+        aRepeat = "N";
+        aRingtone = "DEFUALT RINGBELL";
     }
 
-    public void setTime(long d) {
-        aCal.setTimeInMillis(d);
-    }
 
-    public void setTime(int year, int month, int day, int hour, int minute) {
-        aCal.clear();
-        aCal.set(year, month, day, hour, minute, 0);
-    }
 
     public String showAInfo() {
-        return "id : " +aId + " status : " + aStatus + " time : " + sdf.format(aCal.getTime());
+        return   "id : " +aId
+                + " title : " + aTitle
+                + " status : " + aStatus
+                + " time : " + aSdf.format(aCal.getTime())
+                + " content : " + aContent
+                + " enable : " + aEnable
+                + " repeat : " + aRepeat
+                + " ringtone : " + aRingtone ;
+
     }
 
+    /* getter */
     public int getStatus() {
         return aStatus;
     }
-
     public long getTimeinMillis() {
         return aCal.getTimeInMillis();
     }
-
     public long getId() {
         return aId;
     }
+    public String getTitle() { return aTitle;}
+    public String getContent() { return aContent; }
+    public String getRingtone() { return aRingtone ;}
+    public String getEnable() { return aEnable ;}
+    public String getRepeat() { return aRepeat ;}
+    public String getTime(SimpleDateFormat sdf ) { return sdf.format(aCal.getTime());}
+    public String getTime() { return aSdf.format(aCal.getTime());}
 
+    /* setter */
     public void setId(long id) {
         aId = id;
     }
-
     public void setStatus(int stat)
     {
         aStatus = stat;
     }
+    public void setTitle(String t) { aTitle = t;}
+    public void setContent(String c) {aContent = c;}
+    public void setTime(long d) {
+        aCal.setTimeInMillis(d);
+    }
+    public void setTime(int year, int month, int day, int hour, int minute)
+    {
+        aCal.clear();
+        aCal.set(year, month, day, hour, minute, 0);
+    }
+    public void setRingtone( String r) { aRingtone = r;}
+    public void setEnable ( String e ) { aEnable = e;}
+    public void setRepeat(String r) {aRepeat = r;}
+
 
 
 
--- Application/app/src/main/java/com/alarmate/application/MainActivity.java
+++ Application/app/src/main/java/com/alarmate/application/MainActivity.java
@@ -4,21 +4,32 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Color;
 import android.os.Build;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Switch;
+import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 //    private AlarmManager alarmManager;
     private ViewPager vp;
     public static int i= 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,12 +74,15 @@ public class MainActivity extends AppCompatActivity {
         btn2.setTag(1);
         btn3.setTag(2);
 
+
+
 //        alarmManager = (AlarmManager)this.getSystemService(Context.ALARM_SERVICE);
 //        setAlarm(14,19,10,false);
 //        setAlarm(14,19,40,false);
 //        setAlarm(14,19,59,false);
 
     }
+
 //
 //
 //    private boolean setAlarm(int hour, int min, int days, boolean rept){
@@ -92,4 +106,15 @@ public class MainActivity extends AppCompatActivity {
 //        );
 //        return true;
 //    }
+
+    /* 2017-05-03 : onStart에서 디비 접속 */
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+
+
+    }
+
+
 }
