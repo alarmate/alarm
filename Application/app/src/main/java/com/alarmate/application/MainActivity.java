b2a010ae7389f8c79ee2cd909d7b134f470be6ba
lsj234027 <lsj234027@gmail.com>
2017-05-05 21:12:54 +0900

    conflict

--- Application/app/src/main/java/com/alarmate/application/MainActivity.java
+++ Application/app/src/main/java/com/alarmate/application/MainActivity.java
@@ -18,17 +18,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-
 import android.widget.LinearLayout;
-
 import android.widget.TextView;
-
-<<<<<<< HEAD
 import com.astuetz.PagerSlidingTabStrip;
-
-=======
 import java.util.ArrayList;
->>>>>>> 1a2dec68c8feba09b126e328ade2cb0519d2a5cd
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
@@ -44,12 +37,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        vp = (ViewPager)findViewById(R.id.vp);
-        ll = (LinearLayout)findViewById(R.id.ll);
+        vp = (ViewPager) findViewById(R.id.vp);
+        ll = (LinearLayout) findViewById(R.id.ll);
 
         vp.setAdapter(new PageAdapter(this.getSupportFragmentManager()));
         vp.setCurrentItem(0);
-<<<<<<< HEAD
 //        View.OnClickListener movePageListener = new View.OnClickListener(){
 //            @Override
 //            public void onClick(View v)
@@ -93,16 +85,13 @@ public class MainActivity extends AppCompatActivity {
 //            }
 //        };
 //        vp.addOnPageChangeListener(vpChngListener);
-=======
-        View.OnClickListener movePageListener = new View.OnClickListener(){
+        View.OnClickListener movePageListener = new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
                 int tag = (int) v.getTag();
                 int i = 0;
-                while(i<3)
-                {
-                    if(tag==i)ll.findViewWithTag(i).setSelected(true);
+                while (i < 3) {
+                    if (tag == i) ll.findViewWithTag(i).setSelected(true);
                     else ll.findViewWithTag(i).setSelected(false);
                     i++;
                 }
@@ -111,18 +100,18 @@ public class MainActivity extends AppCompatActivity {
 
         };
 
-        btn1.setOnClickListener(movePageListener);
-        btn2.setOnClickListener(movePageListener);
-        btn3.setOnClickListener(movePageListener);
-        btn1.setTag(0);
-        btn2.setTag(1);
-        btn3.setTag(2);
+//        btn1.setOnClickListener(movePageListener);
+//        btn2.setOnClickListener(movePageListener);
+//        btn3.setOnClickListener(movePageListener);
+//        btn1.setTag(0);
+//        btn2.setTag(1);
+//        btn3.setTag(2);
+//
+//
+//        btn1.setSelected(true);
 
 
-        btn1.setSelected(true);
-
-
-        ViewPager.OnPageChangeListener vpChngListener = new ViewPager.OnPageChangeListener(){
+        ViewPager.OnPageChangeListener vpChngListener = new ViewPager.OnPageChangeListener() {
 
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
@@ -132,14 +121,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onPageSelected(int position) {
                 int i = 0;
-                while(i<3)
-                {
-                    if(position==i)
-                    {
+                while (i < 3) {
+                    if (position == i) {
                         ll.findViewWithTag(i).setSelected(true);
-                    }
-                    else
-                    {
+                    } else {
                         ll.findViewWithTag(i).setSelected(false);
                     }
                     i++;
@@ -148,19 +133,24 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onPageScrollStateChanged(int state) {
->>>>>>> 1a2dec68c8feba09b126e328ade2cb0519d2a5cd
 
-        tabStrip = (PagerSlidingTabStrip)findViewById(R.id.tab_strip);
-        tabStrip.setViewPager(vp);
+                tabStrip = (PagerSlidingTabStrip) findViewById(R.id.tab_strip);
+                tabStrip.setViewPager(vp);
+            }
+        };
+    }
 
+    /* 2017-05-03 : onStart에서 디비 접속 */
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
 
-//        alarmManager = (AlarmManager)this.getSystemService(Context.ALARM_SERVICE);
-//        setAlarm(14,19,10,false);
-//        setAlarm(14,19,40,false);
-//        setAlarm(14,19,59,false);
 
     }
 
+
+}
 //
 //
 //    private boolean setAlarm(int hour, int min, int days, boolean rept){
@@ -184,15 +174,3 @@ public class MainActivity extends AppCompatActivity {
 //        );
 //        return true;
 //    }
-
-    /* 2017-05-03 : onStart에서 디비 접속 */
-    @Override
-    protected void onStart()
-    {
-        super.onStart();
-
-
-    }
-
-
-}
